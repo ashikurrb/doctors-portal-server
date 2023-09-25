5ac902904b8ea6ba0282c075ca82e47ded50905e
Ashikur Rahman Bhuiyan <ashikurrb@outlook.com>
2023-09-25 09:37:04 +0600

    update
    code updated

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 
 //middleWare
 app.use(cors());
+app.use(express.json());
 
 //mongoDB Connection Credentials setup
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.menwp.mongodb.net/?retryWrites=true&w=majority`;
@@ -20,7 +21,44 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    console.log("database connected successfully");
+    const database = client.db('doctors_portal')
+    const appointmentsCollection = database.collection('appointments')
+    const usersCollection = database.collection('users');
+
+    app.get('/appointments', async (req, res) => {
+      const email = req.query.email;
+      const date = new Date(req.query.date).toLocaleDateString();
+      const query = { email: email, date:date }
+      const cursor = appointmentsCollection.find(query);
+      const appointments = await cursor.toArray();
+      res.json(appointments)
+    })
+
+    app.post('/appointments', async (req, res) => {
+      const appointment = req.body;
+      const result = await appointmentsCollection.insertOne(appointment)
+      res.json(result);
+    })
+
+    app.post ('/users', async(req, res)=>{
+      const user = req.body; 
+      const result = await usersCollection.insertOne(user)
+      console.log(result);
+      res.json(result);
+    })
+
+    app.put('/users', async(req, res)=>{
+      const user = req.body;
+      const filter = {email: user.email}
+      const options = {upsert: true}
+      const updateDoc = {$set: user}
+      const result = await usersCollection.updateOne(filter, updateDoc, options)
+      console.log(result);
+      res.json(result);
+    })
+
+
+
   } finally {
     // await client.close();
   }
